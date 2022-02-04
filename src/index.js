8462ada6ca84256329dad0862e0e4232553ef4b8
altynai91 <lenta.kg@gmail.com>
2022-02-05 02:47:13 +0300

    docs: update all files

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.substring(i, i + 10);
+        res+=decodeLetter(letter);
+    }
+
+    return res;
+}
+
+String.prototype.leftTrim = function() {
+    return this.replace(/^0+/,"");
+};
+
+function decodeLetter(letter) {
+    let res = '';
+
+    letter = letter.leftTrim();
+
+    for (let i = 0; i < letter.length; i += 2) {
+        if (letter[i] === '*') {
+            return ' ';
+        }
+
+        if (letter[i] === '1') {
+            if (letter[i + 1] === '1') {
+                res += '-';
+            }
+            else res += '.';
+        }
+        else break;
+    }
+
+    return MORSE_TABLE[res];
 }
 
 module.exports = {
     decode
-}
+};
